ecfa8e4797db7f11a367648e68460a88e75b416c
Gurkan <hello@gurkz.me>
2026-09-13 15:37:40 +0200

    delete elysia server

--- Source/Util/assert.ts
+++
@@ -1,5 +0,0 @@
-
-export default function assert<T>(value: T, message?: string): NonNullable<T> {
-	if (!value) throw new Error(message ?? "Assertion failed!");
-	return value;
-}
--- Source/Util/id.ts
+++
@@ -1,9 +0,0 @@
-import { ulid } from "ulid";
-
-/**
- * utility function to generate an id (its an util function in case we want to change any of the inputs to ULID)
- * @returns a random ID
- */
-export function generateId() {
-	return ulid();
-}
